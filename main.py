d96bd54f00057a7b38122c145f614feb2d38adf9
ripwin <sathiya.kirushnapillai@heig-vd.ch>
2018-04-19 14:40:26 +0200

    Add the main file

---
+++ main.py
@@ -0,0 +1,10 @@
+# VTK - Labo 4 - Scanner d'un genou
+# Author : Sathiya Kirushnapillai Mathieu Monteverde
+
+import vtk
+
+def main():
+    print("VTK Labo 4")
+
+if __name__ == "__main__":
+   main()
